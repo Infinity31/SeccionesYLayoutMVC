c9c78b6cbc51b3e6521cda3cd499d0920cb73bdc
Raquel <rk.infinito31@gmail.com>
2018-03-14 09:14:23 +0100

    Initial commit

---
+++ SeccionesYLayout/Controllers/SeccionesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeccionesYLayout.Controllers{
+    public class SeccionesController : Controller{
+        // GET: Secciones
+        public ActionResult Opcional(){
+            return View();
+        }
+    }
+}
---
+++ SeccionesYLayout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeccionesYLayout.MvcApplication" Language="C#" %>
